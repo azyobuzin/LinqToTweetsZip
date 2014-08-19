db654e074c8266839234d07b988e922fa5112841
azyobuzin <azyobuzin@users.sourceforge.jp>
2014-08-20 00:05:08 +0900

    Dispose できてなかった

---
+++ LinqToTweetsZip/JavaScriptConvert.cs
@@ -0,0 +1,23 @@
+﻿using System.IO;
+using Newtonsoft.Json;
+
+namespace LinqToTweetsZip
+{
+    internal static class JavaScriptConvert
+    {
+        internal static T Read<T>(this ITweetsZipSource source, params string[] entryName)
+        {
+            using (var sr = new StreamReader(source.GetEntryStream(entryName)))
+            {
+                var c = sr.Read();
+                while (c != '=')
+                {
+                    if (c == -1) throw new InvalidDataException();
+                    c = sr.Read();
+                }
+
+                return JsonSerializer.CreateDefault().Deserialize<T>(new JsonTextReader(sr));
+            }
+        }
+    }
+}
